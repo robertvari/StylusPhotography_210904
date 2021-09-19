874219031b6d22e0459c4868a8e4fccafa8a1caf
Robert Vari <mail.pythonsuli@gmail.com>
2021-09-19 14:52:12 +0200

    update

--- Pages/models.py
+++ Pages/models.py
@@ -11,5 +11,32 @@ class PageInfo(models.Model):
     facebook = models.URLField(max_length=200, blank=True)
     instagram = models.URLField(max_length=200, blank=True)
 
+    class Meta:
+        verbose_name_plural = "Oldal infó"
+
     def __str__(self):
         return f"{self.title}: {self.subtitle}"
+
+
+class HomePage(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    subtitle = models.CharField(max_length=200, blank=True)
+    content = models.TextField(max_length=2000, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Home page infó"
+
+    def __str__(self):
+        return self.title
+
+
+class AboutPage(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    subtitle = models.CharField(max_length=200, blank=True)
+    content = models.TextField(max_length=2000, blank=True)
+
+    class Meta:
+        verbose_name_plural = "About page infó"
+
+    def __str__(self):
+        return self.title
