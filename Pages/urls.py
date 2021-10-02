d0eb5eae1c4018b722912f8f8f5a7f6c9ece751d
Robert Vari <mail.pythonsuli@gmail.com>
2021-10-02 16:05:27 +0200

    update

--- Pages/urls.py
+++ Pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import HomeView, AboutView, ContactView
+from .views import HomeView, AboutView, ContactView, ContactSentView
 
 
 urlpatterns = [
     path('', HomeView.as_view()),
     path('contact/', ContactView.as_view()),
     path('about/', AboutView.as_view()),
+    path('contact/sent/', ContactSentView.as_view()),
 ]
