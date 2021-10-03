24996c0a25b8244f389ca6d162e1cac8d2c6a908
Robert Vari <mail.pythonsuli@gmail.com>
2021-10-03 16:46:16 +0200

    update

--- StylusPhotography/settings.py
+++ StylusPhotography/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['robertvari.pythonanywhere.com']
 
 
 # Application definition
